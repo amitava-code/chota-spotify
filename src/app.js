d8936f53e06d05bb80830813de8929a2d31e4a6b
Delta Student <217562233+amitava-code@users.noreply.github.com>
2026-07-03 19:02:58 +0530

    feat(auth): add user registration with password hashing and JWT

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const cookieParser = require("cookie-parser")
+const authRoutes = require("./routes/auth.routes")
 
 
 const app = express()
@@ -7,6 +8,8 @@ const app = express()
 app.use(express.json())
 app.use(cookieParser())
 
+
+app.use("/api/auth", authRoutes)  //Prefix
 
 
 
