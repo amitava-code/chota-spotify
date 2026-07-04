cbf3a5f2e8d0fcd3aa8527ad94cf3a7bf646e85b
Delta Student <217562233+amitava-code@users.noreply.github.com>
2026-07-04 08:07:37 +0530

    feat(routes): protect upload and album routes with authArtist

--- src/routes/muisc.routes.js
+++ src/routes/muisc.routes.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const musicController = require("../controllers/music.controller")
 const multer = require("multer")
+const authMiddleware = require("../middleware/auth.middleware")
 
 const upload = multer({
     storage: multer.memoryStorage()
@@ -9,9 +10,9 @@ const upload = multer({
 const router = express.Router()
 
 
-router.post("/upload", upload.single("music"), musicController.createMusic)
+router.post("/upload", authMiddleware.authArtist, upload.single("music"), musicController.createMusic)
 
-router.post("/album", musicController.createAlbum)
+router.post("/album",authMiddleware.authArtist, musicController.createAlbum)
 
 
 module.exports = router
